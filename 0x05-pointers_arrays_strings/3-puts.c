b984420f8ed85c736f8b8a6a121ad033a4f0354c
muhammed kanyi <contact@kanyi.gm>
2022-07-12 09:11:08 -0700

    fix betty style

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,8 @@
  * _puts - void _puts(char *str);
  * @str: one char type argument
  *
- * Description: function that prints a string, followed by a new line, to stdout
+ * Description: function that prints a string, 
+ * followed by a new line, to stdout
  * Return: none
  */
 void _puts(char *str)
