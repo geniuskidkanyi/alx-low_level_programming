72ddb4a140cd271ae7203598ea378940e07d69ed
muhammed kanyi <contact@kanyi.gm>
2022-07-12 08:53:37 -0700

    fix the line issue

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@
  * reset_to_98 - function with void reset_to_98(int *n);
  * @n: int type pointer n
  *
- * Description: Write a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * Description: Write a function that takes a pointer
+ * to an int as parameter and updates the value it points to to 98
  * Return: value
  */
 void reset_to_98(int *n)
