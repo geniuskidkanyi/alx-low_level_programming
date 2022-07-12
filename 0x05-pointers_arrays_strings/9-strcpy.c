477f3bfdbfff28fa142a2de9a0423b9245fa2580
muhammed kanyi <contact@kanyi.gm>
2022-07-12 09:58:40 -0700

    fix bug

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	i = 0;
+	while (src[i] != '\0')
+	{
 		dest[i] = src[i];
-	for ( ; i < src[i]; i++)
-		dest[i] = '\0';
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
