c0b3235b315ca96ebc071d0057b361a5f52cc3c6
muhammed kanyi <contact@kanyi.gm>
2022-07-12 08:44:20 -0700

    did task for 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "vschool.h"
+
+/**
+ * reset_to_98 - function with void reset_to_98(int *n);
+ * @n: int type pointer n
+ *
+ * Description: Write a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * Return: value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
