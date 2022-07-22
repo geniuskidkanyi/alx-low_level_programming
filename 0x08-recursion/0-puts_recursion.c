6b8fbef0bb0cb58f01fef741567948e3101de62e
muhammed kanyi <contact@kanyi.gm>
2022-07-21 18:18:24 -0700

    fixed betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,10 +3,10 @@
 /**
  * A function that prints a string, followed by a new line
  * @s: string
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == 0)
